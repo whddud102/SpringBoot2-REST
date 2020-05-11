55ff80c99be2cc6f220da5fff5609aa484b11971
JongYoung <whddud102@naver.com>
2020-05-11 17:38:58 +0900

    기능 추가
    BoardRestController.java
    - 생성, 수정, 삭제 동작 추가

--- rest-web/src/main/java/com/community/rest/controller/BoardRestController.java
+++ rest-web/src/main/java/com/community/rest/controller/BoardRestController.java
@@ -7,11 +7,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.hateoas.PagedResources;
 import org.springframework.hateoas.PagedResources.PageMetadata;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
@@ -36,5 +35,30 @@ public class BoardRestController {
         .getBoards(pageable)).withSelfRel());
 
         return ResponseEntity.ok(resources);
+    }
+    
+    // 게시글 생성 메서드
+    @PostMapping
+    public ResponseEntity<?> postBoard(@RequestBody Board board) {
+        board.setCreatedDateNow();
+        boardRepository.save(board);
+        return new ResponseEntity<>("{}", HttpStatus.CREATED);
+    }
+
+
+    // 게시글 수정 메서드
+    @PutMapping("/{idx}")
+    public ResponseEntity<?> putBoard(@PathVariable("idx") Long idx, @RequestBody Board board) {
+        Board persistBoard = boardRepository.getOne(idx);
+        persistBoard.update(board);
+        boardRepository.save(persistBoard);
+        return new ResponseEntity<>("{}", HttpStatus.OK);
+    }
+
+    // 게시글 삭제 메서드
+    @DeleteMapping("/{idx}")
+    public ResponseEntity<?> deleteBoard(@PathVariable("idx") Long idx) {
+        boardRepository.deleteById(idx);
+        return new ResponseEntity<>("{}", HttpStatus.OK);
     }
 }
